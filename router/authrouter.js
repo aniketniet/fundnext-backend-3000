36cf285a2f72d008d65bb65c7057e5c4e6e36e50
Kashish <kashish.biserwal@protonmail.com>
2024-10-04 13:46:33 +0530

    added routes

--- router/authrouter.js
+++ router/authrouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { signup, login, logout, getMessagesByRoomId} = require('../controller/authcontroller');
+const { signup, login, logout, getMessagesByRoomId, sendOtp, verifyOtp, updatePassword} = require('../controller/authcontroller');
 
 router.post('/signup', signup);
 router.post('/login', login);
@@ -8,6 +8,9 @@ router.post('/logout', logout);
 // router.get('/user/:id', getUserById);
 // router.put('/user/:id', updateUser);
 // router.delete('/user/:id', deleteUser);
+router.post('/send-otp', sendOtp)
+router.post('/verify-otp', verifyOtp)
+router.post('/update-password', updatePassword)
 
 
 module.exports = router;
