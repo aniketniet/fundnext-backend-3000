e3d0e1c714ed5208075f1d21621fedec31733422
Aniket Kumar Rathaur <aniketniet@gmail.com>
2024-10-14 14:11:19 +0530

    change in contact

--- model/contact.js
+++ model/contact.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String, // Remove required: true to make it optional
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["genral", "service"],
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String, // Remove required: true to make it optional
-  },
-});
+
+  { timestamps: true } // Enable timestamps
+);
 
 const Contact = mongoose.model("Contact", contactSchema);
 module.exports = Contact;
